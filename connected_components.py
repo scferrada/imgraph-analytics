c0b1570d7213c00301b9f80582b82e57a5779b68
Sebastian Ferrada <scferradaa@gmail.com>
2017-08-16 17:06:49 -0300

    TSV to igraph. Connected components computation.

---
+++ connected_components.py
@@ -0,0 +1,18 @@
+import igraph as ig
+import argparse, os
+
+parser = argparse.ArgumentParser(description='Reads IMGpedia TSV files and prodices igraph lgl')
+
+parser.add_argument('input_graph', type=str, help='the lgl file of the graph')
+#parser.add_argument('output_folder', type=str, help='the directory where the results must be stored')
+#parser.add_argument('--ext', dest='extension', type=str, default='graph', help='the extension of the CSV files, .graph by default')
+
+args = parser.parse_args()
+
+print("Reading graph")
+graph = ig.Graph.Read_Lgl(args.input_graph)
+print("Computing connectivity")
+components = graph.components()
+print("Graph: " + str(graph))
+print("%d connected components" % len(components.subgraphs()))
+print("Giant component: " + str(components.giant()))
---
+++ make_igraph.py
@@ -0,0 +1,26 @@
+import igraph as ig
+import argparse, os
+
+parser = argparse.ArgumentParser(description='Reads IMGpedia TSV files and prodices igraph lgl')
+
+parser.add_argument('input_folder', type=str, help='the directory where the graph files are')
+parser.add_argument('output_folder', type=str, help='the directory where the results must be stored')
+parser.add_argument('--ext', dest='extension', type=str, default='graph', help='the extension of the CSV files, .graph by default')
+
+args = parser.parse_args()
+
+files = []
+for p, d, f in os.walk(args.input_folder):
+	files.extend([os.path.join(p,x) for x in f if x.endswith(args.extension)])
+	break
+print("Loading TSVs")
+edges = []	
+for filename in files:
+	for line in open(filename, 'r').readlines():
+		if "\t2\t" in line:
+			parts = line.split("\t")
+			edge.append((int(parts[0]), int(parts[1])))
+print("Making Graph")
+graph = ig.Graph(edges)
+print("Saving graph")
+graph.write_lgl("hog.lgl")
